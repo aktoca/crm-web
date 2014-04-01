ef656da2627b2db76bd5319213c46c3c5e2aa622
aktoca <axtoca14@gmail.com>
2014-04-01 15:36:01 -0400

    add contact page

--- crm.rb
+++ crm.rb
@@ -10,3 +10,19 @@ end
 get "/contacts" do
   "here are your contacts (hard code)"
 end
+
+
+get "/add_contact" do
+  "Make a contact"
+end
+
+get "/modify_contact" do
+  "Modify existing contact"
+end
+
+get "/one_contact" do
+  "View one contact"
+end
+get "/contacts_by_attribute" do
+get "/delete_contact" do
+get "/exit" do
